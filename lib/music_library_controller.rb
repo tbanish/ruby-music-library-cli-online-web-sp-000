164f61a8fc98a2d4ca1687855d742fa1918e1082
Tom Banish <tombanish@gmail.com>
2020-04-20 19:55:09 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -26,7 +26,6 @@ class MusicLibraryController
   end
   
   def list_songs
-    binding.pry
     sorted = Song.all.sort_by {|s| s.name}
     sorted.each.with_index(1) do |song, index|
       puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
