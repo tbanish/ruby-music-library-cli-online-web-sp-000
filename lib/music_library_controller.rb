b6765cc67a8c9011f572ec89588139fa6115d857
Tom Banish <tombanish@gmail.com>
2020-04-20 19:42:40 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -26,7 +26,7 @@ class MusicLibraryController
   end
   
   def list_songs
-    Song.all.sort_by(&:name).each.with_index(1)
+    Song.all.sort_by(&:name).each.with_index(1) do |song, index|
   end
   
   
